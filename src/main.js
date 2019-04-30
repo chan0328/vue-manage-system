6a9de5de7c1cae9db8266242fa867d6da22efeca
Alex <215783155@qq.com>
2019-04-30 18:06:18 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,30 +1,33 @@
 import Vue from 'vue';
-import axios from 'axios';
 import Quill from 'quill';
+import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import utils from './jslib/utils';
 import ElementUI from 'element-ui';
 import store from './store/store.js';
-import vuePicturePreview from 'vue-picture-preview';//图片预览
-import {ImageExtend} from 'quill-image-extend-module';//富文本图片配置
-import "babel-polyfill";
-import './jslib/filter';
-import './router/authority';
-import './jslib/directives';
-import './assets/css/icon.css';
+import vuePicturePreview from 'vue-picture-preview';
+import {ImageExtend} from 'quill-image-extend-module';
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 // import '../static/css/theme-green/index.css';       // 浅绿色主题
+import './assets/css/icon.css';
+import './assets/css/iconfont.css';
+import './jslib/directives';
+import 'babel-polyfill';
+import './router/authority';
+import './jslib/filter';
+// import './assets/css/iconfontTest.css';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, {
     size: 'small'
 });
-
 Vue.use(vuePicturePreview);
 
-Vue.prototype.$utils= utils;
 Vue.prototype.$axios = axios;
+Vue.prototype.$utils= utils;
+// 公共七牛
+Vue.prototype.$qiniuUrl = 'http://qiniu.mayeeyy.com/';
 
 Quill.register('modules/ImageExtend', ImageExtend);
 
